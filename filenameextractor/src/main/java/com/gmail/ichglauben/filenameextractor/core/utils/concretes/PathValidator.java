b1684361b753f297ffcb0ca57fdbb2bd85e8faac
quauab <ichglauben@gmail.com>
2016-05-08 19:29:08 -0400

    Added comments to the PathValidator class

--- filenameextractor/src/main/java/com/gmail/ichglauben/filenameextractor/core/utils/concretes/PathValidator.java
+++ filenameextractor/src/main/java/com/gmail/ichglauben/filenameextractor/core/utils/concretes/PathValidator.java
@@ -3,7 +3,16 @@ package com.gmail.ichglauben.filenameextractor.core.utils.concretes;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-
+/**	
+ * This class reports whether or not the given file exists.
+ *	@see com.gmail.ichglauben.core.utils.concretes.PathValidator
+ *	@see java.nio.file.Files
+ *	@see java.nio.file.Path
+ *	@see java.nio.file.Paths
+ *	@author	Rick Walker
+ *	@version 0.1
+ *	@since 5/8/2016
+*/
 public class PathValidator {
 	public static boolean pathExists(String filePath) {
 		Path path = null;
